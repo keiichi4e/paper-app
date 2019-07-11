391df7c3a8539e095d6a63e97e1a4769341680ce
Keiichi Inoue <keiichi@toreta.in>
2019-07-11 19:22:54 +0900

    Add menu

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 1440, height: 900});
+  win = new BrowserWindow({ width: 1440, height: 900 });
   win.loadFile('index.html');
   // win.webContents.openDevTools()
   win.on('closed', () => {
@@ -12,7 +12,24 @@ function createWindow() {
   });
 }
 
-app.on('ready', createWindow)
+function createMenu() {
+  var menu = Menu.buildFromTemplate([
+    {
+      label: 'Paper',
+      submenu: [
+        { role: 'quit' },
+      ],
+    },
+    { role: 'editMenu' },
+    { role: 'windowMenu' },
+  ]);
+  Menu.setApplicationMenu(menu);
+};
+
+app.on('ready', function () {
+  createWindow();
+  createMenu();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -33,4 +50,4 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-})
+});
